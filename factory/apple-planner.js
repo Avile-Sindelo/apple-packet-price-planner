db96c0f6895913cd25760e0b165efd846760f1ee
Avile Sindelo <avilesindelo@gmail.com>
2023-05-28 14:26:04 +0200

    completing the widget structure, styling the widget, starting to write the factory function

--- factory/apple-planner.js
+++ factory/apple-planner.js
@@ -1,11 +1,35 @@
-function ApplePlanner(){
+function ApplePlanner(currentState){
     //state variable
-
+    var appleState = currentState || {
+        boxCost: 0,
+        applesInBox: 0,
+        applePacketSize: 0,
+        requiredProfitPercentage: 0
+    }
     //setters
     
     //getters
+    function getNumberOfPackets(){
+
+    }
+
+    function getAppleCost(){
+        
+    }
+
+    function getPacketCost(){
+
+    }
+
+    function getRecommendedPacketPrice(){
+
+    }
 
     return {
         //exposed closure functions
+        getNumberOfPackets,
+        getAppleCost,
+        getPacketCost,
+        getRecommendedPacketPrice
     }
 }
---
+++ tests/cases/apple-planner-test.js
@@ -0,0 +1,7 @@
+describe('Apple planner tester', function(){
+    it('first test case', function(){
+        //initiate an object from the factory function
+        //call the closure methods and pass the relevant params, if any.
+        //assert.equal 
+    });
+});
